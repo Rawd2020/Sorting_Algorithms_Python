bcfb963608aa406e1e4de77c12b7c3ec88a819a6
Rory Williams Doyle <alexander.williamsdoyle26@mail.dcu.ie>
2018-07-15 21:47:44 +0100

    Small styling edits made.

--- Insertion_Sort.py
+++ Insertion_Sort.py
@@ -1,5 +1,5 @@
 def insertion_sort(input_list):
-    for index in range(1, len(input_list)):
+    for index in range(1, len(input_list)): 
         current_value = input_list[index]
         while index > 0 and input_list[index - 1] > current_value:
             input_list[index] = input_list[index - 1]
